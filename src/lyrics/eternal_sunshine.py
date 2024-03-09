f7b06581ca572b4b3a4c225f5e08e2b392ca6efe
jaydenpiao <jaydenpiao@gmail.com>
2024-03-08 16:07:00 -0800

    eternal sunshine

---
+++ src/lyrics/eternal_sunshine.py
@@ -0,0 +1,709 @@
+lyrics = '''
+How can I tell if I'm in the right relationship?
+Aren't you really supposed to know that shit?
+Feel it in your bones and own that shit
+I don't know
+Then I had this interaction I've been thinking 'bout for like five weeks
+Wonder if he's thinking 'bout it too and smiling
+Wonder if he knows that that's been what's inspiring me
+Wonder if he's judging me like I am right now
+I don't care
+I'd rather tell the truth (truth)
+Than make it worse for you, mmm (you)
+
+If the sun refused to shine
+Baby, would I still be your lover?
+Would you want me there?
+If the moon went dark tonight
+And if it all ended tomorrow
+Would I be the one on your mind, your mind, your mind?
+
+And if it all ended tomorrow
+Would you be the one on mine?
+
+Bye-bye
+Boy, bye
+Bye-bye
+Boy, bye
+It's over, it's over, oh yeah
+
+This ain't the first time I've been hostage to these tears
+I can't believe I'm finally moving through my fears
+At least I know how hard we tried, both you and me
+Didn't we?
+Didn't we?
+
+So I grab my stuff
+Courtney just pulled up in the driveway
+It's time
+
+Bye-bye
+Boy, bye
+Bye-bye
+It's over, it's over, oh yeah
+Bye-bye
+I'm taking what's mine
+Yeah
+Bye-bye
+It's over, it's over, oh yeah
+
+Ya know?
+I'm stronger than I think
+Usually, I'd join you on the floor
+But this dance ain't for me
+Boy, just turn the music up
+Maybe someday we'll look back with love
+Didn't think you'd lose me
+Now it's just too late to choose me
+
+So I grab my stuff
+Courtney just pulled up in the driveway
+It's time (it's time)
+
+Bye-bye
+Boy, bye (boy, bye)
+Bye-bye
+It's over, it's over, oh yeah
+Bye-bye
+I'm taking what's mine, yeah (taking what's mine)
+Bye-bye
+It's over, it's over, oh yeah
+
+Now it's certainly bittersweet
+This hook feels too hard to sing
+But it's better than repeat (than repeat, than repeat, than repeat)
+
+Bye-bye (bye-bye)
+Boy, bye (boy, bye)
+Bye-bye
+It's over, it's over, oh yeah (bye)
+Bye-bye (bye-bye)
+I'm taking what's mine (taking what's mine)
+Bye-bye
+It's over, it's over, oh yeah
+
+Bye-bye (bye-bye)
+Boy, bye
+Bye-bye
+It's over, it's over, oh yeah (bye)
+Bye-bye (bye-bye)
+I'm taking what's mine, yeah (I'm taking what's mine)
+Bye-bye
+It's over, it's over, oh yeah
+
+I fall asleep crying
+You turn up the tv
+You don't wanna hear me
+One more sleepless night
+Big day in the morning
+So I take my time to self-soothe
+
+I'm too much for you
+So I really gotta do
+The thing I don't wanna do
+No, I don't, don't, don't, don't
+
+I don't wanna fuck with your head
+It's breaking my heart
+To keep breaking yours again (yours again, yours again)
+This situationship has to end
+But I just can't refuse
+I don't wanna break up again, baby (up, up again, up again)
+
+I made it so easy
+Spent so much on therapy
+Blamed my own co-dependency
+But you didn't even try
+When you finally did, it was at the wrong time
+Won't abandon me again for you and I, I
+
+I'm too much for you
+So I really gotta do
+The thing I don't wanna do
+No, I don't, don't, don't, don't
+
+I don't wanna fuck with your head
+It's breaking my heart
+To keep breaking yours again (yours again, yours again)
+This situationship has to end
+But I just can't refuse
+I don't wanna break up again (up, up again, up again)
+
+We both know it's time
+So we say goodbye
+Just one kiss goodbye
+With tears in our eyes
+Hope you won't, won't regret me
+Hope you'll still think fondly
+Of our little life
+
+I don't wanna fuck with your head
+It's breaking my heart
+To keep breaking yours again (yours again, yours again)
+This situationship has to end
+But I just can't refuse
+I don't wanna break up again, baby (up, up again, up again)
+
+Don't wanna break up again
+I don't wanna, baby
+Situationship has to end
+Know it has to end
+I just can't refuse
+
+When we're all born, Saturn's somewhere
+And the Saturn cycle takes around, about twenty-nine years
+That's when we gotta wake up and smell the coffee because if we've just been sort of
+Relying on our cleverness or relying, you know, just kind of floating along
+Saturn comes along and
+Hits you over the head, hits you over the head, hits you over the head and says
+"Wake up!"
+It's time for you to get real about life and sort out who you really are
+
+Wake up!
+Get real
+
+I
+I don't care what people say
+We both know I couldn't change you
+I guess you could say the same
+Can't rearrange truth
+I've never seen someone lie like you do
+So much, even you start to think it's true
+Ooh
+Get me outta this loop, yeah
+Yeah
+
+So now we play our separate scenes
+Now, now she's in my bed, laying on your chest
+Now I'm in my head
+Wondering how it ends
+
+I'll be the first to say, "I'm sorry"
+Now you got me feeling sorry
+I showed you all my demons, all my lies
+Yet you played me like atari
+Now it's like I'm looking in the mirror
+Hope you feel alright when you're in her
+I found a good boy, and he's on my side
+You're just my eternal sunshine
+Sunshine
+
+So I try to wipe my mind
+Just so I feel less insane
+Rather feel painless
+I'd rather forget than know
+Know for sure
+What we could've fought through behind this door
+Hmm-mmm
+So I close it and move, yeah
+Yeah
+
+So now we play our separate scenes
+Now, now he's in your bed, laying on my chest
+Now I'm in my head
+Wondering how it ends, ends, ends
+
+I'll be the first to say, "I'm sorry"
+Now you got me feeling sorry
+I showed you all my demons, all my lies
+Yet you played me like atari
+Now it's like I'm looking in the mirror
+Hope you feel alright when you're in her
+I found a good boy, and he's on my side
+You're just my eternal sunshine
+Sunshine
+
+Won't break
+Can't shake
+This fate
+Rewrite
+Deep breaths
+Tight chest
+Life, death
+Rewind
+
+Won't break
+Can't shake
+This fate
+Rewrite
+Deep breaths
+Tight chest
+Life, death
+
+I'll be the first to say, "I'm sorry"
+Now you got me feeling sorry
+I showed you all my demons, all my lies
+Yet you played me like atari
+Now it's like I'm looking in the mirror
+Hope you feel alright when you're in her
+I found a good boy, and he's on my side
+You're just my eternal sunshine
+Sunshine
+
+Rewind
+Won't break
+Can't shake
+This fate
+Rewrite
+Deep breaths
+Tight chest
+Life, death
+Rewind
+
+Won't break
+Can't shake
+This fate
+Rewrite
+Deep breaths
+Tight chest
+Life, death
+
+Don't want nobody else around me
+Just need you right here
+You're like the only thing that I see
+It's crystal clear, and I
+
+I want you to come claim it, I do
+What are you waiting for?
+Yeah, want you to name it, I do
+Want you to make it yours
+
+It's like supernatural
+This love's possessing me
+But I don't mind at all
+It's like supernatural
+It's taking over me
+Don't wanna fight the fall
+
+It's like supernatural
+This love's possessing me
+But I don't mind at all
+It's like supernatural
+This love's possessing me
+But I don't mind at all
+
+Need your hands all up on my body
+Like the moon needs the stars
+Nothing else felt this way inside me
+Boy, let's go too far
+
+I want you to come claim it, I do
+What are you waiting for?
+Yeah, want you to name it, I do
+Want you to make it yours
+
+It's like supernatural
+This love's possessing me
+But I don't mind at all
+It's like supernatural
+It's taking over me
+Don't wanna fight the fall
+
+It's like supernatural
+(This love's possessing me but I don't mind at all) no, I don't even mind at all
+It's like supernatural
+(This love's possessing me but I don't mind at all) no, I don't even mind at all
+
+Yeah
+
+I want you to come claim it, I do
+You to come name it, I do
+
+This love's possessing me
+But I don't mind at all
+It's like supernatural
+It's taking over me
+Don't wanna fight the fall
+
+It's like supernatural
+(This love's possessing me but I don't mind at all) no, I don't even mind at all
+It's like supernatural
+(This love's possessing me but I don't mind at all) no, I don't even mind at all
+It's like supernatural
+
+This is a true story about all the lies
+You fantasized 'bout you and I
+This is a true story about all the games
+I know you play
+No, this is not what I need (gimme love, love, gimme love)
+Not what I want (gimme love, love, gimme love)
+Ain't gonna happen to me
+Gimme love, love, gimme love, love
+Love, gimme love, gimme love, love
+Love, love, love
+
+I'll play the villain if you need me to
+I know how this goes, yeah
+I'll be the one you pay to see, play the scene
+Roll the cameras, please
+
+Turning like a dime (dime)
+Wasting all their time
+Sneaking like a creep in the night
+But I'll play whatever part you need me to
+Mm-mm-mmm
+
+This is a true story about all the lies
+You fantasized 'bout you and I
+This is a true story about all the games
+I know you play
+No, this is not what I need (gimme love, love, gimme love)
+Not what I want (gimme love, love, gimme love)
+Ain't gonna happen to me
+Gimme love, love, gimme love, love
+Love, gimme love, gimme love, love
+Love, love, love
+
+I'll play the bad girl if you need me to
+If it makes you feel better
+I'll be the one you love to hate, can't relate
+Too much on my plate
+
+See it in your eyes (eyes)
+You got too much time
+For fun you like to pray for my demise, mm-mmm
+But I'll play whatever part you need me to
+And I'll be good in it too
+
+This is a true story about all the lies
+You fantasized 'bout you and I
+This is a true story about all the games
+I know you play
+No, this is not what I need (gimme love, love, gimme love)
+Not what I want (gimme love, love, gimme love)
+Ain't gonna happen to me (gimme love, love, gimme love, love)
+Ooh-ah (love, gimme love, gimme love, love)
+Gimme love, gimme love (love, love, love)
+
+Not what I want (gimme love, love, gimme love)
+Not what I need (gimme love, love, gimme love)
+Ain't gonna happen to me (gimme love, love, gimme love, love)
+Gimme love, gimme love (love, gimme love, gimme love, love)
+Love, love, love
+
+How can it be you and me?
+Might be meant to be
+Can't unsee it
+But I don't wanna cause no scene
+I'm usually so unproblematic
+So independent
+Tell me why
+'Cause the boy is mi-ine
+
+Something about him is made for somebody like me
+Baby, come over, come over
+And God knows I'm trying, but there's just no use in denying
+
+The boy is mine
+I can't wait to try him
+Let's get intertwined
+The stars, they aligned
+The boy is mine
+Watch me take my time
+I can't believe my mind
+The boy is divine
+Boy is mine
+
+Please know this ain't what I planned for
+Probably wouldn't bet a dime or my life on
+There's gotta be a reason why
+My girls, they always come throughin a sticky situation
+Say, "it's fine" ("it's fine")
+Happens all the ti-ime
+
+Something about him is made for somebody like me
+Baby, come over, come over
+And God knows I'm trying, but there's just no use in denying
+
+The boy is mine
+I can't wait to try him
+Let's get intertwined
+The stars, they aligned
+The boy is mine
+Watch me take my time
+I can't believe my mind
+The boy is divine
+Boy is...
+
+And I know it's simply meant to be
+And I
+I take full accountability for all these tears
+Promise you I'm not usually
+Like this, shit, it's like news to me
+To me
+But I can't ignore my heart, boy!
+
+The boy is mine
+I can't wait to try him
+Let's get intertwined
+The stars, they aligned
+The boy is mine
+Watch me take my time
+I can't believe my mind
+The boy is divine
+Boy is mine
+
+In case you haven't noticed
+Well, everybody's tired
+And healing from somebody
+Or something we don't see just right
+
+Boy, come on, put your lipstick on (no one can tell you nothing)
+Come on and walk this way through the fire (don't care what's on their mind)
+And if you find yourself in a dark situation
+Just turn on your light and be like
+
+"Yes, and?"
+Say that shit with your chest, and
+Be your own fuckin' best friend
+Say that shit with your chest
+Keep moving like, "what's next?"
+"Yes, and?" yeah
+
+Now I'm so done with caring
+What you think, no, I won't hide
+Underneath your own projections
+Or change my most authentic life
+
+Boy, come on, put your lipstick on (no one can tell you nothing)
+Come on and walk this way through the fire (don't care what's on their mind)
+And if you find yourself in a dark situation
+Just turn on your light and be like
+
+"Yes, and?"
+Say that shit with your chest, and
+Be your own fuckin' best friend
+Say that shit with your chest
+Keep moving like, "what's next?"
+"Yes, and?" yeah
+
+My tongue is sacred, I speak upon what I like
+Protected, sexy, discerning with my time (my time)
+Your energy is yours and mine is mine
+What's mine is mine
+My face is sitting, I don't need no disguise
+Don't comment on my body, do not reply
+Your business is yours and mine is mine
+Why do you care so much whose **** I ride?
+Why?
+
+"Yes, and?" ("yes, and? ")
+Say that shit with your chest and (say that shit with your chest)
+Be your own fuckin' best friend (be your own, be your own)
+Say that shit with your chest (say that shit)
+Keep moving like, "what's next?" (with your chest now, baby)
+"Yes, and?"
+
+"Yes, and?" (hoo)
+Say that shit with your chest and (hoo)
+Be your own fuckin' best friend (be your own)
+Say that shit with your chest
+Keep moving like, "what's next?"
+"Yes, and?" yeah
+
+I didn't think you'd understand me
+How could you ever even try?
+I don't wanna tiptoe, but I don't wanna hide
+But I don't wanna feed this monstrous fire
+Just wanna let this story die
+And I'll be alright
+
+We can't be friends
+But I'd like to just pre-etend
+You cling to your papers and pens
+Wait until you like me again
+Wait for your love
+My love, I'll wait for your love
+
+Me and my truth, we sit in silence
+Mmm, baby girl, it's just me and you
+'Cause I don't wanna argue, but I don't wanna bite
+My tongue, yeah, I think I'd rather die
+You got me misunderstood
+But at least I look this good
+
+We can't be friends
+But I'd like to just pre-etend
+You cling to your papers and pens
+Wait until you like me again
+Wait for your love
+My love, I'll wait for your love
+I'll wait for your love
+My love, I'll wait for your love
+
+Know that you made me
+I don't like how you paint me, yet I'm still here hanging
+Not what you made me
+It's something like a daydream
+But I feel so seen in the night
+So for now, it's only me
+And maybe that's all I need
+
+We can't be friends
+But I'd like to just pre-etend
+You cling to your papers and pens
+Wait until you like me again
+Wait for your love
+My love, I'll wait for your love
+I'll wait for your love
+My love, I'll wait for your love
+
+I'll wait for your love
+I'll wait for your love
+I'll wait for your love
+I'll wait for your love
+I'll wait for your love
+
+Hung all my clothes in the closet you made
+Your shoes still in boxes, I send them your way
+Hoping life brings you no new pain
+I rearrange my memories, I try to rewrite our life
+
+But no matter how I try to
+And no matter how I want to
+And no matter how easy things could be if I did
+And no matter how guilty I still feel saying it
+
+I wish I hated you
+I wish that weren't true
+Wish there was worse to you
+I wish you were worse to me
+Yeah, I wish I hated you
+
+Our shadows dance in a parallel plane
+Just two different endings, you learn to repair
+And I learn to keep me in one place
+So close and yet so far if only we had known from the start
+
+But no matter how I try to
+And no matter how I want to
+And no matter how easy things could be if I did
+And no matter how guilty I still feel saying it
+
+I wish I hated you
+I wish that weren't true
+Wish there was worse to you
+I wish you were worse to me
+Yeah, I wish I hated you
+
+
+My boy
+Come take my hand
+Throw your guitar and your clothes in the back seat
+My love
+They don't understand
+But I'll hold your hurt in the box here beside me
+
+How could we know
+We'd rearrange all the cosmos, mmm?
+We crashed and we burned
+Now I just can't go where you don't go
+And usually I'm
+
+Fucked up
+Anxious
+Too much
+But I'll love you like you need me to
+Imperfect for you
+
+Messy
+Completely
+Distressed
+But I'm not like that since I met you
+Imperfect for you
+
+Let's go tonight
+'Cause there's just a few seconds left till tomorrow
+We have
+All that we need
+Let's leave our baggage and wine at the table
+
+Mmm, how could we know
+That this was a happy disaster, mmm? (happy disaster)
+I'm glad we crashed and burned (we crashed and burned)
+I'll never forget when you told me
+You said, "usually I'm
+
+"Fucked up
+Anxious
+Too much
+But I'll love you like you need me to
+Imperfect for you"
+
+Messy
+Completely
+Distressed
+But I'm not like that since I met you
+
+Mmm, how could we know
+We'd make the bad stuff delightful, mmm?
+I'm glad we crashed and burned
+I know we'll be there for each other
+When we're feeling
+
+Fucked up
+Anxious
+Too much
+But I'll love you like you need me to
+Imperfect for you
+
+Messy
+Completely
+Distressed
+But I'm not like that since I met you
+Imperfect for you
+
+Fucked up
+Anxious
+Too much
+Like you need me to
+Imperfect for you
+
+Messy
+Completely
+Distressed
+Not like that since I met you
+Imperfect for you
+
+
+We could go pop all the champagne in California
+We could have omakase in Tokyo if you wanna
+Hypothetically we could do anything that we like
+We could hit up all the slopes in matching snowsuits
+We could hang out at the louvre all night if you want to
+We could spend every dime
+But I don't want anything but more time
+
+No matter what we do
+There's never gonna be an ordinary thing
+No ordinary things with you
+It's funny but it's true
+There's never gonna be an ordinary thing
+As long as I'm with you
+
+You hit just like the first sip of wine after a long day
+You hit like my biggest fan when I hear what the critiques say
+You hit just like a green light when I'm stuck runnin' real late
+I don't need no diamonds, just your time
+
+No matter what we do
+There's never gonna be an ordinary thing
+No ordinary things with you
+It's funny but it's true
+There's never gonna be an ordinary thing
+As long as I'm with you
+
+Ordinary thing, ordinary things with you
+Ordinary thing, as long as I'm with you
+
+And when he'd come home and I'd see him, when he first gets off that train
+It was like God almighty arrived
+It was like seeing daylight
+I mean, I could've packed up and left a million times, you know, it's not that we never fought
+You can overcome that, you know? it-it's very easy, and as I told her
+Never go to bed without kissin' goodnight
+That's the worst thing to do, don't ever, ever do that
+And if you can't, and if you don't feel comfortable doing it
+You're in the wrong place, get out
+
+'''
